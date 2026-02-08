5adf24fedc1a01411cf537debcbeb1edfddd9174
SWARATHMICA <swarathmica@gmail.com>
2026-02-08 20:11:18 +0530

    chore: initialize inventory management project

---
+++ Inventory.java
@@ -0,0 +1,5 @@
+public class Inventory {
+    public static void main(String[] args) {
+        System.out.println("Project setup");
+    }
+}
---
+++ src/inventory/Main.java
@@ -0,0 +1,6 @@
+package inventory;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Inventory Management System");
+    }
+}
