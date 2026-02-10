6f2d2b237e81aefa79e7b1ae5ec433529ab84a7c
SWARATHMICA <swarathmica@gmail.com>
2026-02-10 05:48:41 +0530

    override calculate price for nut

--- src/inventory/Nut.java
+++ src/inventory/Nut.java
@@ -5,4 +5,8 @@ public class Nut extends Item {
     public Nut(String type, String name, int count, double price) {
         super(type, name, count, price);
     }
+    @Override
+    public double calculateValue() {
+        return count * price;
+    }
 }
